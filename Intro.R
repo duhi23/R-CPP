958d13729b2b4bd8accb4950c4d10a9ae919664b
duhi23 <diego.21.phs@hotmail.com>
2016-10-21 23:33:05 +0200

    Funciones

--- Intro.R
+++ Intro.R
@@ -11,14 +11,29 @@ library('inline')
 
 sourceCpp("pares.cpp")
 
+# Función compilada (1)
 cppFunction('int suma(int x, int y, int z) {
             int sum = x + y + z;
             return sum;
             }')
 
-suma
+# Función compilada (2)
+cppFunction('int fibonacci(const int x) {
+if (x < 2) return(x);
+return (fibonacci(x - 1)) + fibonacci(x - 2);
+}')
 
+# Función en texto
+cppfib <- 'int fibonacci(const int x) {
+      if (x < 2) return(x);
+      return (fibonacci(x - 1)) + fibonacci(x - 2);
+}'
+
+# Llamado a función 
+fibCpp <- cppFunction(cppfib)
+
+# Ejecución de funciones
 suma(1, 2, 3)
-
-
+fibonacci(8)
+fibCpp(8)
 
